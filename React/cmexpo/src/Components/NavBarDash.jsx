d43eabaf6b13de44ef971e15f7ee6717c16ac4bb
DBK <DBKDEV@outlook.fr>
2024-03-27 20:05:42 +0100

    Changement de nom Navbarre

--- React/cmexpo/src/Components/NavBarDash.jsx
+++ React/cmexpo/src/Components/NavBarDash.jsx
@@ -3,74 +3,74 @@ import '../Styles/Administrateur/Navbar.css'
 import { Link } from 'react-router-dom';
 
 const NavBarDash = () => {
-        const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-        const toggleSidebar = () => {
-            setIsOpen(!isOpen);
-            menuBtnChange(); // appel de la fonction (facultatif)
-        };
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+        menuBtnChange(); // appel de la fonction (facultatif)
+    };
 
-        const menuBtnChange = () => {
-            // Définissez ici la fonction menuBtnChange si nécessaire
-        };
+    const menuBtnChange = () => {
+        // Définissez ici la fonction menuBtnChange si nécessaire
+    };
 
-        return (
-            <>
-                <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-                    <div className="logo-details">
-                        <i className='bx bxl-c-plus-plus icon'></i>
-                        <div className="logo_name">CMAPP</div>
-                        <i className='bx bx-menu' id="btn" onClick={toggleSidebar}></i>
-                    </div>
-                    <ul className="nav-list">
-                        <li>
-                            <a href="#">
-                                <i className='bx bx-grid-alt'></i>
-                                <span className="links_name">Dashboard</span>
-                            </a>
-                            <span className="tooltip">Dashboard</span>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className='bx bx-user'></i>
-                                <span className="links_name">User</span>
-                            </a>
-                            <span className="tooltip">User</span>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className='bx bx-chat'></i>
-                                <span className="links_name">Messages</span>
-                            </a>
-                            <span className="tooltip">Messages</span>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className='bx bx-pie-chart-alt-2'></i>
-                                <span className="links_name">Analytics</span>
-                            </a>
-                            <span className="tooltip">Analytics</span>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className='bx bx-folder'></i>
-                                <span className="links_name">File Manager</span>
-                            </a>
-                            <span className="tooltip">Files</span>
-                        </li>
-                        <li className="profile">
-                            <div className="profile-details">
-                                <div className="name_job">
-                                    <div className="name">Déconnexion</div>
-                                    <div className="job">Dashboard</div>
-                                </div>
-                            </div>
-                            <i className='bx bx-log-out' id="log_out"></i>
-                        </li>
-                    </ul>
+    return (
+        <>
+            <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+                <div className="logo-details">
+                    <i className='bx bxl-c-plus-plus icon'></i>
+                    <div className="logo_name">CMAPP</div>
+                    <i className='bx bx-menu' id="btn" onClick={toggleSidebar}></i>
                 </div>
-            </>
-        )
-    }
+                <ul className="nav-list">
+                    <li>
+                        <a href="#">
+                            <i className='bx bx-grid-alt'></i>
+                            <span className="links_name">Dashboard</span>
+                        </a>
+                        <span className="tooltip">Dashboard</span>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className='bx bx-qr-scan'></i>
+                            <span className="links_name">Scan Code</span>
+                        </a>
+                        <span className="tooltip">Messages</span>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className='bx bx-user'></i>
+                            <span className="links_name">Exposants</span>
+                        </a>
+                        <span className="tooltip">Exposants</span>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className='bx bx-list-ul'></i>
+                            <span className="links_name">Visiteurs</span>
+                        </a>
+                        <span className="tooltip">Visiteurs</span>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className='bx bx-folder'></i>
+                            <span className="links_name">File Manager</span>
+                        </a>
+                        <span className="tooltip">Files</span>
+                    </li>
+                    <li className="profile">
+                        <div className="profile-details">
+                            <div className="name_job">
+                                <div className="name" id='deco'>Déconnexion</div>
+                                <div className="job">Dashboard</div>
+                            </div>
+                        </div>
+                        <i for="deco" className='bx bx-log-out' id="log_out"></i>
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
+}
 
-    export default NavBarDash ;
+export default NavBarDash;
